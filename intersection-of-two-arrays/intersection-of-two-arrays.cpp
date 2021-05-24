8a31b181fd4714b04029dbe9178729f0db22154a
shishirofficial <81156195+shishirofficial@users.noreply.github.com>
2021-05-25 04:35:25 +0530

    Time: 4 ms (90.73%), Space: 10.7 MB (14.20%) - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
     {
-        set<int> s1,s2;
+        unordered_set<int> s1,s2;
         vector<int> ans;
-        for(int i=0;i<nums1.size();i++)
+        for(auto it:nums1)
         {
-            s1.insert(nums1[i]);
+            s1.insert(it);
         }
-        for(int i=0;i<nums2.size();i++)
+        for(auto it:nums2)
         {
-            s2.insert(nums2[i]);
+            s2.insert(it);
         }
         if(s1.size()>s2.size())
         {
             for(auto it:s1)
             {
-                auto it1=s2.find(it);
-                if(it1 != s2.end())
-                {
-                    ans.push_back(it);
-                }
-            }
-        }
-        else if(s2.size()>s1.size())
-        {
-            for(auto it:s2)
-            {
-                auto it2=s1.find(it);
-                if(it2 != s1.end())
+                if(s2.find(it)!=s2.end())
                 {
                     ans.push_back(it);
                 }
@@ -38,13 +26,12 @@ public:
         {
             for(auto it:s2)
             {
-                auto it2=s1.find(it);
-                if(it2 != s1.end())
+                if(s1.find(it)!=s1.end())
                 {
                     ans.push_back(it);
                 }
             }
         }
-        return ans;
+       return ans; 
     }
 };
