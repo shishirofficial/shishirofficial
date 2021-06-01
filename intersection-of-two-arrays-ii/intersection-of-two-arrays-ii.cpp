da634af91622c04047378197a4ef37ef176a9649
shishirofficial <81156195+shishirofficial@users.noreply.github.com>
2021-06-01 08:52:58 +0530

    Time: 32 ms (7.63%), Space: 10.1 MB (56.83%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,50 +1,24 @@
 class Solution {
 public:
-    int bin(vector<int> &nums2,int low,int high,int value)
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
     {
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(nums2[mid]==value)
-            {
-                return mid;
-            }
-            if(nums2[mid]>value)
-            {
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
-            }
-        }
-        return -1;
-    }
-    
-    
-    
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
-    {
-        if(nums2.size()<nums1.size())
-        {
-            return intersect(nums2,nums1);
-        }
         vector<int> ans;
-        
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        
+        int n=nums2.size();
+        vector<bool> a(n,0);
         for(int i=0;i<nums1.size();i++)
         {
-            int curr=nums1[i];
-            
-            int index=bin(nums2,0,nums2.size()-1,curr);
-            if(index != -1)
+            for(int j=0;j<nums2.size();j++)
             {
-                ans.push_back(curr);
-                nums2.erase(nums2.begin()+index);
+                if(a[j]==0)
+                {
+                    if(nums1[i]==nums2[j])
+                    {
+                        ans.push_back(nums1[i]);
+                        a[j]=1;
+                        break;
+                    }
+                }
             }
-            
         }
         return ans;
     }
